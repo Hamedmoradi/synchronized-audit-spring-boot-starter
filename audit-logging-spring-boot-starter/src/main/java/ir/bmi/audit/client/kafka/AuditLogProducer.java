5a73b159b5bd8679c1b5bf7b0ff4c9ee011e82d2
ha.moradi <hamedmoradi.mailsbox@gmail.com>
2022-02-06 14:12:39 +0330

    fixed bug in sending messages  to kafka

--- audit-logging-spring-boot-starter/src/main/java/ir/bmi/audit/client/kafka/AuditLogProducer.java
+++ audit-logging-spring-boot-starter/src/main/java/ir/bmi/audit/client/kafka/AuditLogProducer.java
@@ -5,8 +5,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.ExecutionException;
@@ -30,20 +33,29 @@ public class AuditLogProducer {
 //    @Value("${bmi.audit.consumer.server:consumerServer:8585/consumer/}")
 //    private String consumerServer;
 
-
-    public void sendMessage(String message, String topic) {
+    public void sendMessage(final String message, String topic) {
         log.info("$$$ -> Producing message --> {}", message);
         try {
-            kafkaTemplate.send(message, topic).get(5, TimeUnit.SECONDS);
+            ListenableFuture<SendResult<String, String>> future = this.kafkaTemplate.send(topic, message);
 
-        } catch (ExecutionException | InterruptedException | TimeoutException | RuntimeException exception){
+            future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+                public void onSuccess(SendResult<String, String> result) {
+                    AuditLogProducer.log.info("Message [{}] delivered with offset {}", message, result.getRecordMetadata().offset());
+                }
+                public void onFailure(Throwable ex) {
+                    AuditLogProducer.log.warn("Unable to deliver message [{}]. {}", message, ex.getMessage());
+                }
+            });
+        }catch (RuntimeException exception){
             exception.printStackTrace();
             auditApiCall(message, MessageBuilder.getToken(message), "http://localhost:8585/consumer/");
         }
     }
 
 
+
     private void auditApiCall(String message, String token, String url) {
+        log.info("****************************** API CALL ******************************");
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(token);
         headers.setContentType(MediaType.APPLICATION_JSON);
